bbdc443380d53b347c81eea69236671482935e13
Maki1209 <m.f.solis.benites@gmail.com>
2024-04-22 03:46:01 -0300

    add

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ def main():
     st.text_input ("Ask a question about your documentos:")
 
     with st.sidebar:
-        st.header("Introduce your API Key")
-        azure_api_key = st.text_input("Azure API Key", type="password")
+        #st.subheader("Introduce your API Key")
+        #azure_api_key = st.text_input("Azure API Key", type="password")
         # st.text_input("OpenAI API Key", type="password")
-        st.divider()
+        #st.divider()
         ####
-        st.header("Your documents")
+        st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
