80add400f429178921c8bae27d76a8a3e246b8c8
Maki1209 <m.f.solis.benites@gmail.com>
2024-04-22 04:14:25 -0300

    add

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import os
 import json
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-
-
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.llms import AzureOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 #######################################
 # BACKEND
@@ -28,6 +31,26 @@ def get_text_chunks(text):
 
     chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    vectorstore = FAISS.from_texts(texts=text_chunks,embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = AzureChatOpenAI(
+                deployment_name="gpt4-8k-jim2", #"gpt-3.5-turbo"  # Chatgpt model!
+                #openai_api_type="azure",
+                api_version="2024-02-15-preview",
+                temperature=0.1
+            )
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retrieve=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
 
 
 
@@ -54,10 +77,13 @@ def main():
                                 
                 # Get Text Chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
-
+                #st.write(text_chunks)
 
                 # Create vectorstore
+                vectorstore = get_vectorstore(text_chunks)
+
+                # Create a conversation 
+                conversation =  get_conversation_chain(vectorstore)
 
 
 
